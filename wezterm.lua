bedbb8751da9079b169628da9a1fd82a144d969d
EmilAnkerstjerne <70580136+EmilAnkerstjerne@users.noreply.github.com>
2026-03-15 20:11:31 +0100

    commit

--- wezterm.lua
+++ wezterm.lua
@@ -5,8 +5,8 @@ local config = wezterm.config_builder()
 config.window_decorations = "RESIZE"
 config.color_scheme = "Gruvbox Dark (Gogh)"
 
-config.window_background_opacity = 0.85
-config.macos_window_background_blur = 50
+config.window_background_opacity = 0.60
+config.macos_window_background_blur = 20
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
 
@@ -19,5 +19,20 @@ config.keys = {
 }
 
 config.animation_fps = 30
+
+config.keys = {
+	{
+		key = "Enter",
+		mods = "SHIFT",
+		action = wezterm.action.SendString("\x1b[13;2u"),
+	},
+	{
+		key = "a",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.SpawnCommandInNewWindow({
+			args = { "/Users/emilankerstjerne/.local/bin/vibe" },
+		}),
+	},
+}
 
 return config
